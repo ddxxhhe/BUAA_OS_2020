6ed1f586fde4f34a05249eea15427526d8ee8cb8
xhd <18373480@buaa.com>
2020-03-02 18:02:45 +0000

    lab0

--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-a=1
-while [ $a -le 100 ]
+a=71
+b=41
+while(($a<=100))
 do
-	if [ $a -gt 70 ]	   #if loop variable is greater than 70
-	then
-
-	elif [ $a -gt 40 ]         # else if loop variable is great than 40   
-	then
-	
-	fi
-	           #don't forget change the loop variable
+	rm -r file$a
+	let "a++"
+done
+while(($b<=70))
+do
+	mv file$b newfile$b
+	let "b++"
 done
